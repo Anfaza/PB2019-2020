5b68494bec0e317a269787dcc716616932da3061
Anfaza <42801578+Anfaza@users.noreply.github.com>
2019-09-17 13:59:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+
+/*DIT HEBBEN WE BEHANDELT TIJDENS DE LES
+
+const firstName = 'Anissa'
+const lastName = 'Boufrahi';
+let age = 23;
+
+//Template string  
+let message = `Hallo ik ben ${firstName} ${lastName} en ik ben ${age} jaar oud.`;
+
+
+console.log(message)
+
+const priceOfAnApple = 0.59;
+const numberOfApples = 200;
+const newPrice = priceOfAnApple*numberOfApples;
+
+console.log(newPrice)
+
+const hzMessage = "Please visit the HZ University of applied science"
+const newMessage = hzMessage.replace("applied", "mad");
+
+console.log(newMessage)
+*/
+
+// Opdracht 1 - Les 3 even of oneven
+
+let number = 48%23
+
+if (number === 2){
+    console.log('This is an uneven number!')
+}
+else { console.log('This number is even!')
+}
+
+// Opdracht 2 
+
+let firstMessage = "Programming is not so cool!"
+let removedMes = firstMessage.replace("not","")
+
+console.log(removedMes)
+// Other possibility
+
+console.log(firstMessage.replace("not",""))
+
+//Opdracht 3
+
+const a = 1400
+const b = "Ik woon in Naboo!"
+
+console.log(a===b)
+ 
+/* Het is geen slimme vergelijking omdat B een string is en A een number.
+Daarom zal het antwoord ook false zijn. */
+
+
